0f76c83157b3bda1f9d73aafa15b89a4586c6d3c
vickey <1143604452@qq.com>
2018-01-24 00:09:43 +0800

    django app website

---
+++ jobs/jobs_data_init.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# @date: 2018/1/23 22:58 
+# @name: jobs_data_init
+# @author：vickey-wu
+
+
---
+++ jobs/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class JobsInfo(models.Model):
+    job_title = models.CharField(max_length=30)
+    job_salary = models.CharField(max_length=30)
+    job_requirement = models.TextField()
+    job_addr = models.CharField(max_length=100)
+    job_exp = models.CharField(max_length=20)
+    job_edu = models.CharField(max_length=20)
+    job_tags = models.CharField(max_length=100)
+    company_name = models.CharField(max_length=100)
+    company_employee_num = models.CharField(max_length=20)
+    company_type = models.CharField(max_length=30)
+
